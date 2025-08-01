9d0fdf82825a9cdefb5d62136de80cc01ce6b095
Alex Acebo <aacebowork@gmail.com>
2025-08-01 07:39:08 -0700

    stuff

--- Libraries/Futures.OpenAI/ChatCompletion.cs
+++ Libraries/Futures.OpenAI/ChatCompletion.cs
@@ -4,51 +4,11 @@ namespace Futures.OpenAI;
 
 public class ChatCompletion : Future<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion>, IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion>
 {
+    public readonly IFuture<List<Chat.ChatMessage>> Messages;
+
     public ChatCompletion(Chat.ChatClient client, Chat.ChatCompletionOptions? options = null, CancellationToken cancellation = default) : base(cancellation)
     {
+        Messages = new Future<List<Chat.ChatMessage>>([]);
         Resolver = (args) => client.CompleteChat(args.Item1, args.Item2 ?? options).Value;
-    }
-}
-
-public static partial class IFutureExtensions
-{
-    public static IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion> Function(this IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion> future, string name, string description, BinaryData parameters, bool strict = false)
-    {
-        var tool = Chat.ChatTool.CreateFunctionTool(
-            name,
-            description,
-            parameters,
-            strict
-        );
-
-        return new Future<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion>(args =>
-        {
-            var options = args.Item2 ?? new();
-            options.Tools.Add(tool);
-            return future.Next(args.Item1, options);
-        });
-    }
-
-    public static IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion> Options(this IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion> future, Chat.ChatCompletionOptions options)
-    {
-        return new Future<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), Chat.ChatCompletion>(args =>
-        {
-            return future.Next((args.Item1, args.Item2 ?? options));
-        });
-    }
-
-    public static TOut Next<TOut>(this IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), TOut> future, params Chat.ChatMessage[] messages)
-    {
-        return future.Next((messages, null));
-    }
-
-    public static TOut Next<TOut>(this IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), TOut> future, params string[] messages)
-    {
-        return future.Next(messages.Select(m => new Chat.UserChatMessage(m)).ToArray());
-    }
-
-    public static TOut Next<TOut>(this IFuture<(IEnumerable<Chat.ChatMessage>, Chat.ChatCompletionOptions?), TOut> future, IEnumerable<Chat.ChatMessage> messages, Chat.ChatCompletionOptions options)
-    {
-        return future.Next((messages, options));
     }
 }
