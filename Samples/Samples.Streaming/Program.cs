497c127f06edbc1ae4e95f7ee4c7e6c1c0aed94a
Alex Acebo <aacebowork@gmail.com>
2025-08-01 11:42:23 -0700

    Update Program.cs

--- Samples/Samples.Streaming/Program.cs
+++ Samples/Samples.Streaming/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Text.Json;
+
+using Futures.OpenAI;
+
+using OpenAI;
+
+var client = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+var future = new ChatCompletion(client.GetChatClient("gpt-3.5-turbo"))
+    .Stream(chunk => Console.WriteLine("hit..."));
+
+future.Next("hi");
+var message = future.Complete();
+Console.WriteLine(JsonSerializer.Serialize(message));
