785cb4c5787807bf7e18cc02b757916ef49b3b57
Alex Acebo <aacebowork@gmail.com>
2025-08-04 17:07:55 -0700

    Update Program.cs

--- Samples/Samples.Streaming/Program.cs
+++ Samples/Samples.Streaming/Program.cs
@@ -9,13 +9,8 @@ List<OpenAI.Chat.ChatMessage> messages = [];
 var client = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
 var future = Providers.From.ChatStreamAsync(client.GetChatClient("gpt-3.5-turbo")).Storage(messages);
 
-foreach (var update in future.Next("hi"))
+foreach (var _ in future.Next("hi"))
 {
-    Console.WriteLine(JsonSerializer.Serialize(update, new JsonSerializerOptions()
-    {
-        WriteIndented = true,
-        DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
-    }));
 }
 
 Console.WriteLine($"done! {messages.Count}");
